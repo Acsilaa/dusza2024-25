94e374bb1593a454b7be1bf2d7ad33571fae2556
Acsilaa <galeengames@gmail.com>
2024-11-08 20:56:57 +0100

    szépen már cseszd meg

--- dusza_web/controller.py
+++ dusza_web/controller.py
@@ -2,5 +2,11 @@ from urllib.request import Request
 from django.shortcuts import render, redirect
 
 def render_view(request):
+    if(request.user.username):
+        if(request.user.groups.all()[0].name == "Organiser"):
+            return render(request, 'organiser/home.html')
+        if(request.user.groups.all()[0].name == "Principal"):
+            return render(request, 'principal/home.html')
+        if(request.user.groups.all()[0].name == "contestant"):
+            return render(request, 'contestant/home.html')
     return render(request, f'index.html')
-
