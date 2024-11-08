3a600300d207723aea1bd6478e038df50a573f6f
Atemzy <43964339+atemzy@users.noreply.github.com>
2024-11-08 19:43:04 +0100

    team form

--- team/controller.py
+++ team/controller.py
@@ -1,2 +1,13 @@
 from urllib.request import Request
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import TeamCreationForm
+
+def registerTeam(request):
+    form = TeamCreationForm(request.GET)
+    if request.method == "POST":
+        form = TeamCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index") #TODO
+    context = {'form': form}
+    return render(request, f'register.html', context)
--- team/forms.py
+++ team/forms.py
@@ -3,14 +3,26 @@ from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.forms import User
 from django.core.exceptions import ValidationError
 from django.contrib.auth import authenticate, login, logout
+from category.models import Category
+from language.models import Language
+from school.models import School
+
 from .models import Team
 
 class TeamCreationForm(forms.Form):
     name = forms.CharField(label='name', min_length=5, max_length=150)
-    contestant1_name = forms.CharField(label='password',min_length=5, max_length=150 )
-    contestant1_grade = forms.DecimalField(label='password',min_value=9, max_value=15,decimal_places=0,max_length=2 )
-    contestant1_grade = forms.DecimalField(label='password',min_value=9, max_value=15,decimal_places=0,max_length=2 )
-    contestant1_grade = forms.DecimalField(label='password',min_value=9, max_value=15,decimal_places=0,max_length=2 )
+    contestant1_name = forms.CharField(label='1. versenyző neve',min_length=5, max_length=150 )
+    contestant1_grade = forms.DecimalField(label='1. versenyző évfolyama', decimal_places=0,max_digits=2 )
+    contestant2_name = forms.CharField(label='2. versenyző neve',min_length=5, max_length=150 )
+    contestant2_grade = forms.DecimalField(label='2. versenyző évfolyama', decimal_places=0,max_digits=2 )
+    contestant3_name = forms.CharField(label='3. versenyző neve',min_length=5, max_length=150 )
+    contestant3_grade = forms.DecimalField(label='3. versenyző évfolyama', decimal_places=0,max_digits=2 )
+    contestant4_name = forms.CharField(label='Póttag neve',min_length=5, max_length=150 )
+    contestant4_grade = forms.DecimalField(label='Póttag évfolyama', decimal_places=0,max_digits=2 )
+    school = forms.ModelChoiceField(queryset=School.objects.all())
+    teachers = forms.CharField(label='Felkészítő tanár neve',min_length=5, max_length=150 )
+    category =  forms.ModelChoiceField(queryset=Category.objects.all())
+    language =  forms.ModelChoiceField(queryset=Language.objects.all())
 
     def name_clean(self):
         name = self.cleaned_data['name'].lower()
