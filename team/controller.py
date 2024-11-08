8aed09f1b15ba4786f09492371e1c81690b7d45e
Acsilaa <galeengames@gmail.com>
2024-11-08 16:15:54 +0100

    asd

--- team/controller.py
+++ team/controller.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 from category.models import Category
 from .forms import TeamForm
 
@@ -9,17 +9,9 @@ def register(request):
     elif request.method == "POST":
         return processRequest(request)
 
-def showForm(request): #GET
-    form = TeamForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    context = {'form': form}
-    data = {}
-    # get options (category, schools, languages)
-    categories = list(Category.objects.values())
-    data["categories"] = categories
-    print(categories)
-    return render(request, f'register.html', context)
+def showForm(request):
+    form = UserCreationForm()
+    return render(request, 'register.html', {"form":form})
 
 def processRequest(request): #POST
     pass
