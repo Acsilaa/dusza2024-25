ad3f2c22b0759f2559377188e150724f0eb826c9
Atemzy <43964339+atemzy@users.noreply.github.com>
2024-11-08 20:18:59 +0100

    xd

--- team/forms.py
+++ team/forms.py
@@ -19,10 +19,10 @@ class TeamCreationForm(forms.Form):
     contestant3_grade = forms.DecimalField(label='3. versenyző évfolyama', decimal_places=0,max_digits=2 )
     contestant4_name = forms.CharField(label='Póttag neve',min_length=5, max_length=150 )
     contestant4_grade = forms.DecimalField(label='Póttag évfolyama', decimal_places=0,max_digits=2 )
-    school = forms.ModelChoiceField(queryset=School.objects.all())
+    school = forms.ModelChoiceField(queryset=School.objects.all(),initial=Category.objects.first())
     teachers = forms.CharField(label='Felkészítő tanár neve',min_length=5, max_length=150 )
-    category =  forms.ModelChoiceField(queryset=Category.objects.all())
-    language =  forms.ModelChoiceField(queryset=Language.objects.all())
+    category =  forms.ModelChoiceField(queryset=Category.objects.all(),initial=Category.objects.first())
+    language =  forms.ModelChoiceField(queryset=Language.objects.all(),initial=Category.objects.first())
 
     def name_clean(self):
         name = self.cleaned_data['name'].lower()
@@ -31,8 +31,21 @@ class TeamCreationForm(forms.Form):
             raise ValidationError("Name Already Exist")
         return name
 
-    def save(self, commit=True):
+    def save(self,request, commit=True):
         team = Team.objects.create(
             name=self.cleaned_data['name'],
+            contestant1_name=self.cleaned_data['contestant1_name'],
+            contestant1_grade=self.cleaned_data['contestant1_grade'],
+            contestant2_name=self.cleaned_data['contestant2_name'],
+            contestant2_grade=self.cleaned_data['contestant2_grade'],
+            contestant3_name=self.cleaned_data['contestant3_name'],
+            contestant3_grade=self.cleaned_data['contestant3_grade'],
+            contestant4_name=self.cleaned_data['contestant4_name'],
+            contestant4_grade=self.cleaned_data['contestant4_grade'],
+            teachers=self.cleaned_data['teachers'],
+            school=self.cleaned_data['school'],
+            user=request.user,
+            category=self.cleaned_data['category'],
+            language=self.cleaned_data['language'],
         )
         return team
--- team/models.py
+++ team/models.py
@@ -22,4 +22,4 @@ class Team(models.Model):
 
 
     def __str__(self) -> str:
-        return self.user
+        return self.name
