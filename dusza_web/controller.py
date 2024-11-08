37cba84552f89363c4525dc34dc318918d1e17ad
Acsilaa <galeengames@gmail.com>
2024-11-08 20:40:33 +0100

    killme

--- dusza_web/controller.py
+++ dusza_web/controller.py
@@ -1,9 +1,6 @@
 from urllib.request import Request
 from django.shortcuts import render, redirect
 
-def render_view(request, path="index"):
-    try:
-        return render(request, f'{path}.html')
-    except:
-        return render(request, f'index.html')
+def render_view(request):
+    return render(request, f'index.html')
 
