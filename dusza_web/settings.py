9cb60aac11b230f5725ebcb2a2a7cccd81465c23
Atemzy <43964339+atemzy@users.noreply.github.com>
2024-11-08 14:05:23 +0100

    aha

--- dusza_web/settings.py
+++ dusza_web/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #third party
-    'bootstrap5',
+    'django-bootstrap-v5',
     #own apps (django-admin startapp <name>)
     'team',
     'language',
