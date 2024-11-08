117e36a42fe84f7a5aedaa602c0e939f352ef568
Atemzy <43964339+atemzy@users.noreply.github.com>
2024-11-08 17:32:48 +0100

    login

--- User/controller.py
+++ User/controller.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from category.models import Category
-from .forms import UserForm
+from .forms import CustomUserCreationForm
 from django.shortcuts import render, redirect
 
 def registerUser(request):
-    form = UserForm(request.GET)
+    form = CustomUserCreationForm(request.GET)
     if request.method == "POST":
-        form = UserForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return redirect("index") #TODO
+        else:
+            print(form.errors)
+    context = {'form': form}
+    data = {}
+    # get options (category, schools, languages)
+    categories = list(Category.objects.values())
+    data["categories"] = categories
+    print(categories)
+    return render(request, f'register.html', context)
+
+def loginUser(request):
+    form = UserCreationForm(request.GET)
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
             form.save()
