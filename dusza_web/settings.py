ebc77a93e4bb57de9449eccf410990de159b17bf
Acsilaa <galeengames@gmail.com>
2024-11-09 22:48:49 +0100

    settings update

--- dusza_web/settings.py
+++ dusza_web/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for dusza_web project.
 
@@ -11,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os.path
 from pathlib import Path
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
