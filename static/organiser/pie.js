7c61a098e48f0898c9bf39996e39b72b5a15ceaf
Zsolti <113987666+SigMaZsola@users.noreply.github.com>
2024-11-10 16:34:13 +0100

    gfhd

--- static/organiser/pie.js
+++ static/organiser/pie.js
@@ -1,4 +1,4 @@
-
+const lga = document.getElementsByClassName("lga");
 const chart = document.getElementById("pite");
 // datas
 total = 0
@@ -7,17 +7,25 @@ Object.entries(datas).forEach(([key, value]) => {
 })
 layout = ""
 prev_end = 0
+let colors =[]
 Object.entries(datas).forEach(([key, value]) => {
     abc = "0123456789abcdef"
     cc = ""
     for(let i = 0; i<6; i++){
         cc += abc[Math.floor(Math.random() * abc.length)]
     }
+    colors.push(cc)
+
     curr_end = Math.floor(value/total * 100)
     layout += `#${cc} ${prev_end}% ${curr_end}%, `
     prev_end = curr_end
 })
 layout = layout.substring(0, layout.length - 2);
-$(function(){
-    $("#pite").css('background', `conic-gradient(${layout})`)
-})
+
+$("#pite").css('background', `conic-gradient(${layout})`)
+console.log(datas,"asd")
+for(i = 0; i < datas.length; i++){
+    $(lga[i]).css('color',  `#${colors[i]}`)
+    
+}
+
