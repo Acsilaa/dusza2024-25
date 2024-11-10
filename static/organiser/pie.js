dd02e12d42e4be6963a375bcc3c97bd8365af584
Acsilaa <galeengames@gmail.com>
2024-11-10 15:14:34 +0100

    push wee

--- static/organiser/pie.js
+++ static/organiser/pie.js
@@ -1,1 +1,24 @@
-const chart = document.getElementsByClassName("pite")[0];
+const chart = document.getElementById("pite");
+// datas
+total = 0
+Object.entries(datas).forEach(([key, value]) => {
+    total += value*1
+})
+layout = ""
+prev_end = 0
+Object.entries(datas).forEach(([key, value]) => {
+    abc = "0123456789abcdef"
+    cc = ""
+    for(let i = 0; i<6; i++){
+        cc += abc[Math.floor(Math.random() * abc.length)]
+    }
+    curr_end = Math.floor(value/total * 100)
+    layout += `#${cc} ${prev_end}% ${curr_end}%, `
+    prev_end = curr_end
+})
+layout = layout.substring(0, layout.length - 2);
+console.log(chart)
+$(chart).css('background', `conic-gradient(
+      ${layout}
+    );`)
+    console.log(layout)
