60bb440a1a898076277d1268361e408500234426
Atemzy <43964339+atemzy@users.noreply.github.com>
2024-11-10 11:05:06 +0100

    asd

--- static/message.js
+++ static/message.js
@@ -1,6 +1,6 @@
 const messages_El = document.getElementById('messages');
 const filter_El = document.getElementsByClassName('filter')[0];
-
+const hasFilter = filter_El != null;
 
 document.addEventListener('DOMContentLoaded', function(){
     if(messages_El.innerHTML.indexOf("<li") === -1){
@@ -10,8 +10,17 @@ document.addEventListener('DOMContentLoaded', function(){
         messages_El.style.opacity = '0';
         messages_El.remove();
     },3000)
-    console.log(filter_El);
     filter_El.classList.add('start-0');
 
 })
+function showFilter(){
+    if(filter_El.classList.contains('start-0')){
+        filter_El.classList.remove('start-0');
+    }else{
+        filter_El.classList.add('start-0');
+    }
+}
+function search(){
+
+}
 
