47716e3d940ba1d0a96dfdb62551d9a0435c1057
Acsilaa <galeengames@gmail.com>
2024-11-10 16:58:11 +0100

    added colors

--- static/organiser/pie.js
+++ static/organiser/pie.js
@@ -8,24 +8,44 @@ Object.entries(datas).forEach(([key, value]) => {
 layout = ""
 prev_end = 0
 let colors =[]
-Object.entries(datas).forEach(([key, value]) => {
-    abc = "0123456789abcdef"
-    cc = ""
-    for(let i = 0; i<6; i++){
-        cc += abc[Math.floor(Math.random() * abc.length)]
+function inarray(arr, e){
+    for(i = 0; i < arr.length; i++){
+        if(e == arr[i])
+            return true
     }
-    colors.push(cc)
+    return false
+}
+it = 0
+_colors = [
+    "22577a",
+    "38a3a5",
+    "57cc99",
+    "80ed99",
+    "c7f9cc",
+    "FFFFFC",
+    "757780",
+    "D5ACA9",
+    "0892A5",
+    "BB7E5D",
+    "CE4760",
+    "6369D1",
+    "34435E",
+    "60E1E0"
+];
+Object.entries(datas).forEach(([key, value]) => {
+    index = Math.floor(Math.random() * _colors.length)
+    console.log(index)
+    cc = _colors[it]
 
     curr_end = Math.floor(value/total * 100)
-    console.log(curr_end)
     layout += `#${cc} ${prev_end}% ${prev_end + curr_end}%, `
                         
-                        
+    colors.push(cc)
     $('ul.datas').append(`<li class="lga">${key}</li>`)
     prev_end += curr_end
+    it++
 })
 layout = layout.substring(0, layout.length - 2);
-console.log(layout)
 $("#pite").css('background', `conic-gradient(${layout})`)
 console.log(colors)
 for(i = 0; i < colors.length; i++){
